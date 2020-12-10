26fbbd41bb8814c8e55196c87d995b1a076f8127
Benjamin Hoipkemier <benjamin.hoipkemier@maritzcx.com>
2020-12-10 08:03:16 -0500

    Day 9-10

---
+++ DayCodeBase/Day10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.DayCodeBase
+{
+	public class Day10 : DayCodeBase
+	{
+		public override string Problem1()
+		{
+			var data = GetData(2)
+				.Select(ulong.Parse)
+				.OrderBy(l => l)
+				.ToList();
+			data.Insert(0, 0);
+			var groups = data
+				.Select((num, index) => index == data.Count - 1 ? 3 : data[index + 1] - num)
+				.GroupBy(l => l)
+				.ToList();
+			return (groups.First(g => g.Key == 1).Count() * groups.First(g => g.Key == 3).Count()).ToString();
+		}
+
+		public override string Problem2()
+		{
+			var data = GetData(2)
+				.Select(ulong.Parse)
+				.OrderBy(l => l)
+				.Select(num => (num, possibilities: (ulong?)null))
+				.ToList();
+			data.Insert(0, (0, 1));
+			for (var i = 1; i < data.Count; ++i)
+			{
+				var possibilities =
+					((data[i - 1].num + 3 >= data[i].num) ? data[i - 1].possibilities : 0) +
+					((i - 2 >= 0) && (data[i - 2].num + 3 >= data[i].num) ? data[i - 2].possibilities : 0) +
+					((i - 3 >= 0) && (data[i - 3].num + 3 >= data[i].num) ? data[i - 3].possibilities : 0);
+				data[i] = (data[i].num, possibilities);
+			}
+			return data.Last().possibilities.ToString();
+		}
+	}
+}
---
+++ DayCodeBase/Day9.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.DayCodeBase
+{
+	public class Day9: DayCodeBase
+	{
+		public override string Problem1()
+		{
+			var preambleSize = 25;
+			var data = GetData(1).Select(d => ulong.Parse(d)).ToArray();
+			return GetInvalidNumber(data, preambleSize)?.ToString() ?? "Error";
+		}
+
+		public override string Problem2()
+		{
+			var preambleSize = 25;
+			var data = GetData(1).Select(d => ulong.Parse(d)).ToArray();
+			var invalidNum = GetInvalidNumber(data, preambleSize);
+			for(var i = 0; i < data.Length; ++i)
+			{
+				var count = GetSummationRange(data, i, (ulong)invalidNum);
+				if(count != null)
+				{
+					return (data.Skip(i).Take((int)count).Min() + data.Skip(i).Take((int)count).Max()).ToString();
+				}
+			}
+			return "Error";
+		}
+
+		private int? GetSummationRange(ulong[] data, int startIndex, ulong invalidNum)
+		{
+			ulong sum = 0;
+			for(var count = 1; sum <= invalidNum; ++count)
+			{
+				sum = data.Skip(startIndex).Take(count).Aggregate((ulong)0, (acc, cur) => acc + cur);
+				if (sum == invalidNum) return count;
+			}
+			return null;
+		}
+
+		private ulong? GetInvalidNumber(ulong[] data, int preambleSize) {
+			for (var i = preambleSize; i < data.Length; ++i)
+			{
+				if (!IsValid(new Span<ulong>(data, i - preambleSize, preambleSize), data[i])) return data[i];
+			}
+			return null;
+		}
+
+		private bool IsValid(Span<ulong> span, ulong value)
+		{
+			var preamble = new HashSet<ulong>(span.ToArray());
+			foreach(var num in preamble)
+			{
+				var otherNum = value - num;
+				if (otherNum != num && preamble.Contains(otherNum)) return true;
+			}
+			return false;
+		}
+	}
+}
